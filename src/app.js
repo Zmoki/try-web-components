d7c3ba5361553c78e916079f27b6dbcc38780056
Zarema Khalilova <zarema.khalilova@gmail.com>
2017-01-24 20:02:22 +0300

    Add store and state updating for `todo-list`

--- src/app.js
+++ src/app.js
@@ -1,7 +1,3 @@
 let app = document.getElementById('app')
 
-app.innerHTML = `
-  <hello-message name="Zarema"></hello-message>
-  <hr/>
-  <todo-list></todo-list>
-`
+app.innerHTML = `<hello-message name="Zarema"></hello-message>`
--- src/todo-list.js
+++ src/todo-list.js
@@ -1,16 +1,41 @@
-const state = ['Buy milk', 'Call Sarah', 'Pay bills']
+const store = (() => {
+  let state
+
+
+  return todos => {
+    if (todos) {
+      state = todos
+      render('todo-list', document.getElementById('todoList'))
+    }
+
+    return state
+  }
+})()
+
+const render = (component, parent = document.body) =>
+  parent.innerHTML = `<${component}></${component}>`
 
 customElements.define('todo-item', class extends HTMLElement {
   constructor() {
     super()
     const root = this.attachShadow({mode: 'open'})
 
-    root.innerHTML = ` 
+    root.innerHTML = `
       <label>
         <input type=checkbox>
         <slot></slot>
       </label>
     `
+    root.querySelector('input').addEventListener('click', () =>
+      store(store().filter((todo, index) => index != this.index)))
+  }
+
+  static get observedAttributes() {
+    return ['index']
+  }
+
+  attributeChangedCallback(name, old, change) {
+    this.index = change
   }
 })
 
@@ -19,14 +44,18 @@ customElements.define('todo-list', class extends HTMLElement {
     super()
     const root = this.attachShadow({mode: 'open'})
 
-    root.innerHTML = ` 
+    root.innerHTML = `
       <ul>
-        ${state.map(todo => `
+        ${store().map((todo, index) => `
           <li>
-            <todo-item>${todo}</todo-item>
+            <todo-item index=${index}>
+              ${todo}
+            </todo-item>
           </li>
         `).join('')}
       </ul>
     `
   }
 })
+
+store(['Buy milk', 'Call Sarah', 'Pay bills'])
